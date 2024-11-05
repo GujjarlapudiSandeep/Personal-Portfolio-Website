e433a113a21b00f5ed6904af165edecd54b8d68b
GujjarlapudiSandeep <sandeepgujjarlapudi28@gmail.com>
2024-11-05 14:51:23 +0530

    Added HTML,CSS,JS files

---
+++ index.js
@@ -0,0 +1,21 @@
+function validateForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  if (!name || !email || !subject || !message) {
+    alert("All fields must be filled out!");
+    return false;
+  }
+  return true;
+}
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
